4b5de0302c12f7d85b9e5b551a007f2970d838f7
Yeasir Ahamed <yahamed007@gmail.com>
2020-08-01 01:28:29 +0600

    add log logitem and preloader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import SearchBar from './components/layout/SearchBar';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
     M.AutoInit();
   }, []);
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 };
 
---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+import { AiFillDelete } from 'react-icons/ai';
+
+const LogItem = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <div>
+        <a
+          href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
+        >
+          {log.message}
+        </a>
+        <br />
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span> last updated by{' '}
+          <span className="black-text">{log.tech}</span> on{' '}
+          <Moment format="MMMM DD YYYY, h:mm:ss a">{log.date}</Moment>
+        </span>
+        <a href="#!" className="secondary-content">
+          {' '}
+          <AiFillDelete />{' '}
+        </a>
+      </div>
+    </li>
+  );
+};
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+export default LogItem;
